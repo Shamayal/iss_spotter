5dde8c219b04e4a46b06a732c22ece7437a4eccf
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-16 13:03:27 -0400

    fetch geo coordinates based on IP address

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // index.js
 
 // The code below is temporary and can be commented out.
-const { fetchMyIP , fetchCoordsByIP } = require('./iss');
+// const { fetchMyIP , fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:' , ip);
-});
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// The code below is temporary and can be commented out.
+// const { fetchCoordsByIP } = require('./iss');
+
+// fetchCoordsByIP('99.229.34.76', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
