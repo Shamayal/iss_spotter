50f0d1cc89a50f62dff2c03bded3ef671615b5c2
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-16 12:03:32 -0400

    initial commit

---
+++ iss.js
@@ -0,0 +1,34 @@
+const request = require('request'); // to use request library and make the HTTP request
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+
+    callback(null, ip);
+  
+  });
+
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+
+};
+
+module.exports = { fetchMyIP , fetchCoordsByIP };
